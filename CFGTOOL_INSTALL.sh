bb554e3e6e19834f01ed80c77f963a7e8243bca7
J C Lawrence <claw@kanga.nu>
2015-07-11 23:59:27 -0700

    Install cleanups

--- CFGTOOL_INSTALL.sh
+++ CFGTOOL_INSTALL.sh
@@ -2,6 +2,5 @@
 
 install -d -g root -o root -m 0755 -p /etc/qworkerd/plugins
 install -D -g root -o root -m 0644 -p -t /etc/qworkerd configs/*.templ
-install -D -g root -o root -m 0644 -p -t /etc/qworkerd/plugins configs/sample_plugin.conf
 install -D -g root -o root -m 0644 -p configs/qworkerd.cfgtool /etc/cfgtool/module.d/qworkerd
 install -D -g root -o root -m 0644 -p configs/qworkerd.upstart.conf.templ /etc/init/qworkerd.conf.templ
--- qworkerd/main.py
+++ qworkerd/main.py
@@ -1,16 +1,31 @@
 #! /usr/bin/env python
 
-import logging, os
+import logging, logtool, os
 from celery import Celery
+from celery.signals import celeryd_after_setup, setup_logging
 from django.conf import settings
 
 LOG = logging.getLogger (__name__)
+DEFAULT_LOGCONF = "/etc/qworkerd/logging.conf"
 
 os.environ.setdefault ("DJANGO_SETTINGS_MODULE", "qworkerd.settings")
 app = Celery ("qworkerd", include = ["qworkerd.tasks",])
 app.config_from_object ("django.conf:settings")
 app.autodiscover_tasks (lambda: settings.INSTALLED_APPS)
 app.set_current ()
+
+@setup_logging.connect
+#@logtool.log_call
+def setup_logging_handler (**kwargs): # pylint: disable=W0613
+  logging.config.fileConfig (DEFAULT_LOGCONF,
+                             disable_existing_loggers = False)
+
+@celeryd_after_setup.connect
+# @logtool.log_call
+def setup_direct_queue (sender, instance, **kwargs): # pylint: disable=W0613
+  # sender is the nodename of the worker
+  queue_name = '{0}.dq'.format (sender)
+  instance.app.amqp.queues.select_add (queue_name)
 
 if __name__ == "__main__":
   app.start()
