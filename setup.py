702836ce2b45c7429dfae94cfd582d6ddb8dee8e
J C Lawrence <claw@kanga.nu>
2015-08-06 17:42:24 -0700

    auto-pyver

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
 #! /usr/bin/env python
 
 try:
-  import pyver # pylint: disable=W0611
+  import pyver
 except ImportError:
-  import os, subprocess
-  try:
-    environment = os.environ.copy()
-    cmd = "pip install pyver".split (" ")
-    subprocess.check_call (cmd, env = environment)
-  except subprocess.CalledProcessError:
-    import sys
-    print >> sys.stderr, "Problem installing 'pyver' dependency."
-    print >> sys.stderr, "Please install pyver manually."
-    sys.exit (1)
+  import pip
+  pip.main (['install', 'pyver'])
   import pyver # pylint: disable=W0611
 
 from setuptools import setup, find_packages
